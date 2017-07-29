2d9bfc3ea96626f4a003875efb36f64bcc0ab037
Ivaylo <bloodydoom3@gmail.com>
2017-07-30 02:24:23 +0300

    I changed project from phantomjs to casperjs

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-var page = require('webpage').create();
-const url = 'https://www.jobs.bg/front_job_search.php?distance=0&location_sid=&categories%5B%5D=15&job_type%5B%5D=4&all_position_level=1&all_company_type=1&keyword=';
 
-page.open(url, function (status) {
-    console.log(status);
-    if(status === "success") {
-        page.render('example.png');
-    }
-    phantom.exit();
+var casper = require('casper').create();
+var jobsDomain = 'https://www.jobs.bg/';
+var queryStrings = 'front_job_search.php?distance=0&location_sid=&categories%5B%5D=15&all_type=0&all_position_level=1&all_company_type=1&keyword='
+casper.start(jobsDomain + queryStrings);
+
+casper.on('remote.message', function (msg) {
+    console.log('remote message caught: ' + msg);
 });
+
+casper.then(function() {
+    var jobsStringAll = this.evaluate(function(){
+        var jobsNode = document.querySelectorAll('.offerslistRow a');
+        var jobsString = '';
+        var jobsArray = [].slice.call(jobsNode);
+        jobsArray.map(function(obj){
+            console.log(obj)
+            jobsString += 12;
+        })
+        return jobsString;
+    })
+    //page.render('blabla.png')
+    //console.log(jobsStringAll)
+})
+
+casper.run();
 
