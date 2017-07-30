9f08b2541b20c54e62b02c61b02b1586eba2226a
Ivaylo <bloodydoom3@gmail.com>
2017-07-30 05:31:52 +0300

    add every job offer to an array

--- index.js
+++ index.js
@@ -2,25 +2,31 @@
 var casper = require('casper').create();
 var jobsDomain = 'https://www.jobs.bg/';
 var queryStrings = 'front_job_search.php?distance=0&location_sid=&categories%5B%5D=15&all_type=0&all_position_level=1&all_company_type=1&keyword='
+var jobsRowNodes;
 casper.start(jobsDomain + queryStrings);
 
 casper.on('remote.message', function (msg) {
-    console.log('remote message caught: ' + msg);
-});
+    console.log(msg);
+})
+
+function getJobRows() {
+    var jobsLinkNodes = document.querySelectorAll('.offerslistRow a.joblink');
+    var jobsRowNodes = [];
+
+    for (var i = 0; i < jobsLinkNodes.length; i++) {
+        var currentRow = jobsLinkNodes[i].parentElement.parentElement;
+        var job = currentRow.querySelector('.joblink').innerHTML;
+        var employer = currentRow.querySelector('.company_link').innerHTML;
+        jobsRowNodes.push({job: job, employer: employer});
+    }
+    return jobsRowNodes;
+}
 
 casper.then(function() {
-    var jobsStringAll = this.evaluate(function(){
-        var jobsNode = document.querySelectorAll('.offerslistRow a');
-        var jobsString = '';
-        var jobsArray = [].slice.call(jobsNode);
-        jobsArray.map(function(obj){
-            console.log(obj)
-            jobsString += 12;
-        })
-        return jobsString;
-    })
-    //page.render('blabla.png')
-    //console.log(jobsStringAll)
+    jobsRowNodes = this.evaluate(getJobRows);
+}).then(function () {
+    console.log(jobsRowNodes[1].job);
+    console.log(jobsRowNodes[1].employer);
 })
 
 casper.run();
